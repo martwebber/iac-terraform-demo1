8493e0c47ac4f65854fac6de06d1709d70b53a6f
Martin Mwangi <mwangi.martin24@gmail.com>
2023-03-20 22:00:00 +0300

    Edit jenkins file

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket    = "516610242115-state-file-bucket"
     key       = "iac-terraform-demo1.tfstate"
     region    = "us-east-1"
+    dynamodb_table = "iac-terraform-demo1-dynamodb-table"
     profile   = "aws-ubuntu-local"
   }
 }
